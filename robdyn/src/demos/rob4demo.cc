6b8484cd78acc6ee825d45b24f42c311f980d921
Gavekort <fredrik@saevland.com>
2016-03-23 15:28:47 +0100

    Demo for new robot4

--- robdyn/src/demos/rob4demo.cc
+++ robdyn/src/demos/rob4demo.cc
@@ -25,13 +25,13 @@ int main(int argc, char **argv)
     boost::variate_generator<boost::mt19937&, boost::uniform_real<> > rsize(rng,size_range);
     std::cout << "RNG sanity: " << rlocation() << std::endl;
 
-    //renderer::OsgVisitor v;
+    renderer::OsgVisitor v;
     ode::Environment env(0.0f, 0.0f, 0.0f);
     env.set_gravity(0, 0, -9.81);
     std::cout << env.get_pitch() << " " << env.get_roll() << " " << env.get_z() << std::endl;
-    robot::robot4 hyb(env, Eigen::Vector3d(0, 0, 1));
+    robot::robot4 hyb(env, Eigen::Vector3d(0, 0, 0.2));
     static const float step = atof(argv[4]);
-    //hyb.accept(v);
+    hyb.accept(v);
 
     using namespace boost::assign;
     std::vector<size_t> b_servos = list_of(0)(3)(4)(7);
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
             (new ode::Box(env, Eigen::Vector3d(rlocation(), rlocation(), bsize/2),
                           10, bsize, bsize, bsize));
         g.push_back(b);
-        //b->accept(v);
+        b->accept(v);
         b->fix();
         g.push_back(b);
     }
@@ -57,13 +57,13 @@ int main(int argc, char **argv)
         double dist = std::abs(pos(0)) + std::abs(pos(1)) + std::abs(pos(2));
         std::cout << dist << std::endl;
         x += step;
-        //v.update();
+        v.update();
         env.next_step(step);
         hyb.next_step(step);
         double a = atof(argv[1]);
         double b = atof(argv[2]);
         double c = atof(argv[3]);
-        double phase = a*tanh(4*sin((2*M_PI)*(x+b))) + c;
+        double phase = a*tanh(4*sin((1*M_PI)*(x+b))) + c;
 
        /* for (size_t i = 0; i < hyb.servos().size(); ++i){
             if(i % 2){
@@ -74,7 +74,15 @@ int main(int argc, char **argv)
         }*/
                 std::cout << phase << " " << phase * M_PI/180 << " " << cos(x) << std::endl;
         for (size_t i = 0; i < hyb.servos().size(); ++i){
+        if(i <= 1){ //body joints only
+            hyb.servos()[i]->set_angle(ode::Servo::DIHEDRAL, phase * M_PI/180);
+        }else{
+            if(i == 3 || i == 5 || i == 7 || i == 9){
+                hyb.servos()[i]->set_angle(ode::Servo::DIHEDRAL, (phase * 1.8) * M_PI/180);
+            }else{
                 hyb.servos()[i]->set_angle(ode::Servo::DIHEDRAL, phase * M_PI/180);
+            }
+        }
         }
 }
 return 0;
